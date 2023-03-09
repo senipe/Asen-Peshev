55264eca4db5af8ac3a7529cdcd29e7253ed089b
senipe <asenpeshevuni@gmail.com>
2023-03-09 13:37:01 +0000

    mobile finetuning

--- JavaS/sidebar.js
+++ JavaS/sidebar.js
@@ -20,9 +20,10 @@ function showSideBar() {
 
     setTimeout(function() {
         document.getElementById("sidebar").classList.toggle('active');
+        sidebarTextAnimation()
         if (srcCheck !== 0) {
             setTimeout(function() {
-                document.getElementsByClassName("toggle-btn")[0].src = "Gallery/Graphics/menuIconWhite.png";
+                document.getElementsByClassName("toggle-btn")[0].src = "Gallery/Graphics/menuIconWhite2.png";
                 enableScroll()
                 if (projects_button_interactive.style.display == "block") {
                     projects_button_interactive.style.display = "none";
@@ -36,10 +37,16 @@ function showSideBar() {
                     projects_button_interactive.style.display = "block";
                     projectsbtnCheck = 0;
                 }
-                document.getElementsByClassName("toggle-btn")[0].src = "Gallery/Graphics/menuIconBlack.png";
+                document.getElementsByClassName("toggle-btn")[0].src = "Gallery/Graphics/menuIconBlack2.png";
                 disableScroll()
                 srcCheck = 1;
             }, 0400);
         }
     }, 0001);
+    document.documentElement.scrollIntoView({ behavior: "smooth" });
+}
+
+function sidebarTextAnimation() {
+    document.getElementById("sidebar-default-list").classList.toggle('active');
+    document.getElementById("sidebar-projects-list").classList.toggle('active');
 }
